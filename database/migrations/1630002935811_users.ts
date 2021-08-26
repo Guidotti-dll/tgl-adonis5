0eb4d7dd42beec3c3d1bdf16c333594d1c6d6110
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-08-26 15:50:10 -0300

    feat: add User migration and model,add auth config

---
+++ database/migrations/1630002935811_users.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class UsersSchema extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('name', 80).notNullable()
+      table.string('email', 254).notNullable().unique()
+      table.string('password', 60).notNullable()
+      table.boolean('is_confirmed').defaultTo(false)
+      table.string('reset_token')
+      table.timestamp('token_created_at')
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
