4855a2c7f14902c0f4ccc73b64806b6572bec29b
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-09-16 18:58:52 -0300

    feat: add beforeEach and test unique email

--- __tests__/creatUser.spec.ts
+++ __tests__/creatUser.spec.ts
@@ -1,11 +1,17 @@
 import test from 'japa'
 import User from 'App/Models/User'
-import { JSDOM } from 'jsdom'
 import supertest from 'supertest'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
-test.group('Create User', () => {
+test.group('Create User', (group) => {
+  group.beforeEach(async () => {
+    const users = await User.all()
+    users.forEach(async (user) => {
+      if (user.id !== 1) await user.delete()
+    })
+  })
+
   test('create user and return your id', async (assert) => {
     const { body } = await supertest(BASE_URL).post('/users').send({
       name: 'Lucas',
@@ -13,9 +19,16 @@ test.group('Create User', () => {
       password: '12345678',
       password_confirmation: '12345678',
     })
-
-    console.log(body)
-
     assert.exists(body.id)
+  })
+
+  test('create user with existing email', async (assert) => {
+    const { status } = await supertest(BASE_URL).post('/users').send({
+      name: 'Lucas',
+      email: 'admin@root.com',
+      password: '12345678',
+      password_confirmation: '12345678',
+    })
+    assert.equal(status, 400)
   })
 })
